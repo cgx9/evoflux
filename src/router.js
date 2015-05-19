89707c4f31c9fef124f0d8066bd9585b4aac4db4
guoxing <chenguoxing@kjtpay.com.cn>
2015-05-19 13:19:50 +0800

    路由增加query获取url参数

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@
 var DirectorRouter = require('director');//.Router();
 var isServer = !process.browser;
 var R = isServer ? DirectorRouter.http.Router : DirectorRouter.Router;
-
+if(!isServer) var queryString = require('query-string');
 var Router = function () {
   // this.init = "/";
   this.notfound = "404";
@@ -15,8 +15,15 @@ Router.prototype.__create__ = function(routers){
   
   delete routers["notfound"];
   delete routers["init"];
+  var routerInit = R(routers).configure({notfound: notfoundRoute})//;.init(initRoute);
+  if(!isServer){
+    var parsed = queryString.parse(location.search);
+    routerInit.query = function(key){
+      return parsed[key];
+    }
+  }
   
-  return R(routers).configure({notfound: notfoundRoute})//;.init(initRoute);
+  return routerInit;
 };
 
 module.exports = new Router();
